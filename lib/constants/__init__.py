8ac734e203d3e8111816e1162b99af9405543bee
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2026-09-24 16:12:34 +0800

    feat(constants): added `REVERSED` constants for chassis motors

--- lib/constants/__init__.py
+++ lib/constants/__init__.py
@@ -12,21 +12,25 @@ class ChassisConstants:
         R_EN_PIN = 0
         L_PWM_PIN = 21
         R_PWM_PIN = 20
+        REVERSED = False
 
     class LeftRear:
         L_EN_PIN = 0
         R_EN_PIN = 0
         L_PWM_PIN = 10
         R_PWM_PIN = 11
+        REVERSED = False
 
     class RightFront:
         L_EN_PIN = 0
         R_EN_PIN = 0
         L_PWM_PIN = 19
         R_PWM_PIN = 18
+        REVERSED = True
 
     class RightRear:
         L_EN_PIN = 0
         R_EN_PIN = 0
         L_PWM_PIN = 12
         R_PWM_PIN = 13
+        REVERSED = True
